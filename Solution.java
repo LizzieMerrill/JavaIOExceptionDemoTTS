b40f501c42c60dbe5e1fd0ec3da0f2b03b405914
LizzieMerrill <97261333+LizzieMerrill@users.noreply.github.com>
2022-09-16 18:23:30 -0700

    Update Solution.java
    Finished exception handling project.

--- Solution.java
+++ Solution.java
@@ -1,34 +1,22 @@
 import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-    while(!StdIn.isEmpty()) {
-        try {
-         int num1 = StdIn.readInt();
-         int num2 = br.readInt();
-         if (num1 != (int)num1 || num2 != (int)num2){
-             throw new InputMismatchException();
-         }
-         else if (num2 == 0){
-             throw new ArithmeticException();
-         }
-         else{
-             System.out.println(num1 / num2);
-         }
-      } catch (IOException ioe) {
-         System.out.println(ioe);
-      }
-      catch (InputMismatchException ime) {
-         System.out.println(ime);
-      }
-      catch (ArithmeticException ae) {
-         System.out.println(ae + ": / by zero");
-      }
-    }
-}
+        Scanner scnr = new Scanner(System.in);
+        try{
+        int num1 = scnr.nextInt();
+        int num2 = scnr.nextInt();
+        try{
+        System.out.println(num1/num2);
+        }
+        catch (ArithmeticException ae) {
+        System.out.println(ae);
+        }
+        }
+        catch (InputMismatchException ime) {
+        System.out.println("java.util.InputMismatchException");
+        }
+        }//end of main
+}//end of class
